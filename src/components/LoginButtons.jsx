77393c71e3561f8346d018bf2460b1f0fd4e13f2
Marcin Dziedzinski <m.m.dziedzinski@gmail.com>
2022-12-27 23:48:44 +0100

    firebase login

--- src/components/LoginButtons.jsx
+++ src/components/LoginButtons.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import style from "../scss/components/LoginButtons.module.scss";
 import { NavLink, useResolvedPath, useMatch } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 const UserLogin = () => {
+  const { user, logout } = UserAuth();
+  const navigate = useNavigate();
+  const handleLogout = async () => {
+    try {
+      await logout();
+      navigate("/");
+      console.log("log out");
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
   return (
     <div className={style.container}>
       <NavLink to="/login" className={style.link} end>
@@ -12,6 +25,8 @@ const UserLogin = () => {
         {" "}
         Sign up{" "}
       </NavLink>
+      <p>Your email: {user && user.email}</p>
+      <button onClick={handleLogout}>Log out</button>
     </div>
   );
 };
